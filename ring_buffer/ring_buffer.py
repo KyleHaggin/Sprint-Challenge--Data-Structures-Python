dcfa8f5edd9bb8a1f0fbe3d3d8e5846bd4c364ae
KyleHaggin <KyleHaggin@users.noreply.github.com>
2020-02-28 10:29:47 -0800

    finish ring_buffer.py stretch

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -7,10 +7,6 @@ class RingBuffer:
         self.current = None
         self.storage = DoublyLinkedList()
 
-    # def initialize(self):
-    #     while self.storage.length < self.capacity:
-    #         self.storage.add_to_tail(None)
-
     def append(self, item):
         # Find/create the node
         node = self.storage
@@ -18,8 +14,6 @@ class RingBuffer:
         if self.storage.length < self.capacity:
             # Add value to queue from tail
             node.add_to_tail(item)
-            # Init rest of list as None
-            # self.initialize()
             self.current = node.head
             return
 
@@ -57,10 +51,48 @@ class RingBuffer:
 
 class ArrayRingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.current = None
+        self.storage = DoublyLinkedList()
+        self.initialize()
+
+    def initialize(self):
+        while self.storage.length < self.capacity:
+            self.append(None)
 
     def append(self, item):
-        pass
+        # Find/create the node
+        node = self.storage
+
+        if self.storage.length < self.capacity:
+            # Add value to queue from tail
+            node.add_to_tail(item)
+            # Init rest of list as None
+            self.current = node.head
+            return
+
+        if self.storage.head == self.current:
+            node.remove_from_head()
+            node.add_to_tail(item)
+            self.current = self.storage.tail
+        else:
+            node.remove_from_head()
+            node.add_to_tail(item)
 
     def get(self):
-        pass
+        # Note:  This is the only [] allowed
+        list_buffer_contents = []
+        # Initialize node as self.storage for easy reading
+        node = self.current
+
+        x = 0
+        while x < self.storage.length:
+            if node.value is not None:
+                list_buffer_contents.append(node.value)
+            node = node.next
+            if node is None:
+                node = self.storage.head
+            x += 1
+        # Remove None values form list
+        list_buffer_contents = [i for i in list_buffer_contents if i]
+        return list_buffer_contents
